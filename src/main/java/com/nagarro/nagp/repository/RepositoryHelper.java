b3c1cc45fd6d352ac04d31f90862021d65c85dd3
sahilsharma <sahil.sharma@nagarro.com>
2019-07-17 18:05:28 +0530

    refactored code

--- src/main/java/com/nagarro/nagp/repository/RepositoryHelper.java
+++ src/main/java/com/nagarro/nagp/repository/RepositoryHelper.java
@@ -5,10 +5,10 @@ import com.nagarro.nagp.Inventory;
 public class RepositoryHelper {
 
 	public static Inventory saveFragile(final Inventory inventory) {
-		return null;
+		return inventory;
 	}
 
 	public static Inventory saveDurable(final Inventory inventory) {
-		return null;
+		return inventory;
 	}
 }
